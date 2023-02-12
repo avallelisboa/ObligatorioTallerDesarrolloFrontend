d30d0d410cf54f2a5a885ef5587a5fce2b2a3a25
Andrés <avallelisboa@gmail.com>
2023-02-12 19:54:30 -0300

    store and reducers were created

---
+++ obligatorio/src/features/departmentsSlice.ts
@@ -0,0 +1,22 @@
+import {createSlice} from "@reduxjs/toolkit"
+import Department from "../models/entities/Department"
+
+const initialState = {
+    departments: Department[0]
+}
+
+const departmentsSlice = createSlice({
+    name: "departments",
+    initialState,
+    reducers:{
+        addDepartment: (state,action) =>{
+            state.departments.push(action.payload);
+        },        
+        emptyDepartments: (state) =>{
+            state.departments.splice(0,state.departments.length);
+        }
+    }
+});
+
+export const {addDepartment, emptyDepartments} = departmentsSlice.actions;
+export default departmentsSlice.reducer;
