4889a2631f7647cd788ce6b453a1501e4bc534c4
Andrés <avallelisboa@gmail.com>
2023-02-26 06:04:04 -0300

    react chart was added.

--- obligatorio/src/businessLogic/expenseBL.ts
+++ obligatorio/src/businessLogic/expenseBL.ts
@@ -1,11 +1,21 @@
 import Expense from "../models/entities/Expense";
+import Heading from "../models/entities/Heading";
 import ValidationResult from "../models/validationModels/actionResult";
 
 function isExpenseValid(expense: Expense):ValidationResult{
     return new ValidationResult("", true);
 }
+function totalExpenseByHeadingCalculation(expenses:Array<Expense>, heading:Heading){
+    let totalExpense = 0;
+    expenses.forEach((expense)=>{
+        if(heading.headingId == expense.category)
+            totalExpense += expense.total;
+    });
+    return totalExpense;
+}
 
 const expenseBL ={
-    isExpenseValid
+    isExpenseValid,
+    totalExpenseByHeadingCalculation
 }
 export default expenseBL;
--- obligatorio/src/businessLogic/incomeBL.ts
+++ obligatorio/src/businessLogic/incomeBL.ts
@@ -1,11 +1,21 @@
+import Heading from "../models/entities/Heading";
 import Income from "../models/entities/Income";
 import ValidationResult from "../models/validationModels/actionResult";
 
 function isIncomeValid(income:Income):ValidationResult{
     return new ValidationResult("", true);
 }
+function totalIncomeByHeadingCalculation(incomes:Array<Income>, heading:Heading){
+    let totalIncome = 0;
+    incomes.forEach((income)=>{
+        if(heading.headingId == income.category)
+            totalIncome += income.total;
+    });
+    return totalIncome;
+}
 
 const incomeBL ={
-    isIncomeValid
+    isIncomeValid,
+     totalIncomeByHeadingCalculation
 }
 export default incomeBL;
